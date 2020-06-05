0d127e0bf881683baf45c757bd88e7dc444d5e63
Samarna <63645732+Samarna@users.noreply.github.com>
2020-06-05 15:33:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var canvas;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track;
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-50);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
